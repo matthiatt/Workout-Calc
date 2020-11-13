096452b9590fde18ec4b11822baf6e0bb023c255
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-12 19:45:25 -0600

    edit server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Connections needed
 const express = require("express");
 const mongoose = require("mongoose");
-const mongojs = require("mongojs");
+const morgan = require("morgan");
+// const mongojs = require("mongojs");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,9 +13,20 @@ db.on("error", error =>
   console.log("Database Error:", error);
 });
 
-
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan);
+app.use(express.static("public"));
+app.use(require("./routes/api.js"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// get route set up with HTML.
 app.get("/", (req,res) => 
 {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -28,19 +41,6 @@ app.get("/stats", (req,res) =>
 {
   res.sendFile(path.join(__dirname, 'public', 'stats.html'));
 });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", 
-{
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
