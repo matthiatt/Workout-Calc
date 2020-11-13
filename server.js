420c98dfded102a4dfcdd8bcd7f8c8f1ff32283e
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-12 22:50:42 -0600

    pushing changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(require("./routes/apiRoute"));
-require("./routes/htmlRoutes")(app);
+app.use(require("./routes/htmlRoutes"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
 {
