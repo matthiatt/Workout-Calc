68b49a37bb5062a7c978ea253de073c9a26e4bea
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-12 19:08:34 -0600

    server done. just starting on api.

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mongojs = require("mongojs");
 
 const PORT = process.env.PORT || 3000;
 
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => 
+{
+  console.log("Database Error:", error);
+});
+
+
 const app = express();
+
+app.get("/", (req, res) => 
+{
+  res.send("Welcome");
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
